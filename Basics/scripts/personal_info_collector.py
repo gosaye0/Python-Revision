4783330a1f798ce8cf759f042c8b478f0ab12b71
Gosaye Emshaw <gossaye0@gmail.com>
2025-07-09 13:06:33 +0300

    Initial commit: Python revision

---
+++ Basics/scripts/personal_info_collector.py
@@ -0,0 +1,10 @@
+# Objective: Collect user input, convert types, and output formatted info
+
+user_first_name = input('First Name:')
+if 
+last_name = input('Last Name:')
+age = int(input('Age:'))
+height = float(input('Height in cm:'))
+job = input('Job: ')
+
+print(f'Hi, I amG {first_name} {last_name}, {age} years old, {height} cm tall. I am {job}.')
